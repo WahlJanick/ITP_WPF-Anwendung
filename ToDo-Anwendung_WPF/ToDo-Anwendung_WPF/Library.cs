3dd1c1a02a42ddf81347512b599e614504635c28
Maximilian Moser <maximilian.moser@htlwy.at>
2025-09-22 15:31:14 +0200

    added the library file

---
+++ ToDo-Anwendung_WPF/ToDo-Anwendung_WPF/Library.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics;
+namespace ToDo_Anwendung_WPF {
+	public sealed class Library {
+		private Library() {}
+		public sealed class Time {
+			private Time() {}
+			public static long getSystemReferenceClockNanoSeconds() {
+				return 10000L * Stopwatch.GetTimestamp() / TimeSpan.TicksPerMillisecond * 100L;
+			}
+		}
+	}
+}
