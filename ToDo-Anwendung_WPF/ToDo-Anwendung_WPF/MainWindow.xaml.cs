b3fa1c39689b29398aedaf606c27f93f3819e66e
Sebastian Langer <sebastian.langer@htlwy.at>
2025-09-22 15:23:10 +0200

    SaveToDo() implemented.

--- ToDo-Anwendung_WPF/ToDo-Anwendung_WPF/MainWindow.xaml.cs
+++ ToDo-Anwendung_WPF/ToDo-Anwendung_WPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO.Packaging;
+﻿using System.IO;
+using System.IO.Packaging;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -16,16 +17,42 @@ namespace ToDo_Anwendung_WPF
 	{
 		private void NewToDo(object s, RoutedEventArgs e)
 		{
-			
+			ViewBox.Visibility = Visibility.Collapsed;
+			NewBox.Visibility = Visibility.Visible;
 		}
-		private void saveToDo(object s, RoutedEventArgs e)
+		private void SavaToDo(object s, RoutedEventArgs e)
         {
-			
-		}
+			string[] toDo_str = label.Content.ToString().Split('\n');
+			if (toDo_str == null)
+            {
+				MessageBox.Show("You need to write a Todo.");
+                return;
+            }
+			int amountOfToDos = 0;
+			using (var sr = new StreamReader("ToDo.txt"))
+			{
+				string line;
+				while ((line = sr.ReadLine()) != null)
+				{
+					if (line.Split(':')[0] == "todo")
+						++amountOfToDos;
+				}
+			}
+			using (var sw = new StreamWriter("ToDo.txt"))
+			{
+				sw.WriteLine($"todo:{amountOfToDos + 1}");
+				foreach (string str in toDo_str)
+				{
+					sw.WriteLine(str);
+				}
+			}
+            NewBox.Visibility = Visibility.Collapsed;
+        }
 		private void ViewToDo(object s, RoutedEventArgs e)
 		{
-
-		}
+            NewBox.Visibility = Visibility.Visible;
+            ViewBox.Visibility = Visibility.Collapsed;
+        }
 		public MainWindow()
 		{
 			InitializeComponent();
